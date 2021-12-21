6136d914f2b48b6be08e3939e0f7a9d27169b4af
Damien Biasotto <damien.biasotto@gmail.com>
2021-12-21 19:08:59 +0800

    Bump GHC to 8.10.7 so M1 users can enjoy haskellings too

--- default.nix
+++ default.nix
@@ -1,2 +1,2 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/7e9b0dff974c89e070da1ad85713ff3c20b0ca97.tar.gz") {}, compiler ? "ghc8104" }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/14f3576475069686ed963ddd8d20f1a79e723b07.tar.gz") {}, compiler ? "ghc8107" }:
 pkgs.pkgs.haskell.packages.${compiler}.callPackage ./haskellings.nix { }
